85dc10fdae588dd336616713c6570d339154f7ba
Kartikay Mishra <kartikay.m@rahinfotech.com>
2026-06-23 15:10:39 +0530

    Features 4-8: Parquet output, schema validation, dynamic tables, dedup hashes, daily partitions

--- athena.tf
+++ athena.tf
@@ -7,6 +7,14 @@ resource "aws_athena_workgroup" "this" {
 
     result_configuration {
       output_location = "s3://${aws_s3_bucket.data.bucket}/${var.athena_results_prefix}"
+
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = aws_kms_key.data.arn
+      }
     }
+
+    execution_role = null
+    bytes_scanned_cutoff_per_query = 1073741824
   }
 }
